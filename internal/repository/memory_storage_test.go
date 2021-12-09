efd995d3f144e56cd73b1ad44fe7fe03985562f2
Dmytro Ivanov <ivanovdmytroa@gmail.com>
2021-12-09 02:11:16 +0200

    added tests

---
+++ internal/repository/memory_storage_test.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"testing"
+)
+
+func TestInitActiveUsersStorage(t *testing.T) {
+	InitActiveUsersStorage()
+
+	if GetActiveUsersStorage() == nil {
+		t.Fatal("Failed to initialize memory users storage")
+	}
+}
+
+func TestAddUserToActiveUsersList(t *testing.T) {
+	InitActiveUsersStorage()
+	storage := GetActiveUsersStorage()
+	storage.AddUserToActiveUsersList("user")
+
+	if len(storage.Users) == 0 {
+		t.Fatal("Failed add user to active users list")
+	}
+}
+
+func TestRemoveUserFromActiveUsersList(t *testing.T) {
+	InitActiveUsersStorage()
+	storage := GetActiveUsersStorage()
+	storage.AddUserToActiveUsersList("user")
+
+	if len(storage.Users) == 0 {
+		t.Fatal("Failed add user to active users list")
+	}
+
+	storage.RemoveUserFromActiveUsersList("user")
+
+	if len(storage.Users) > 0 {
+		t.Fatal("Failed delete user from active users list")
+	}
+}
