1eaf53a5726f3b36f29124d9fb20775a45316786
Dmytro Ivanov <ivanovdmytroa@gmail.com>
2022-01-31 22:45:27 +0200

    added pprof reports

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/IvanovDmytroA/lets-go-chat/internal/server"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	server.Start()
 }
